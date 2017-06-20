61241bfeb9fc1ce3ba35d04e138f0ed55f5a2a76
radu <radu.helstern@deskpro.com>
2017-06-20 16:08:52 +0100

    add Deskpro services

---
+++ src/main/javascript/Deskpro/Parsers.js
@@ -0,0 +1,15 @@
+import {MemberInfo} from '../Domain'
+
+export const parseMemberInfo = person =>
+{
+  const { display_name: fullName, emails, primary_email } = person;
+
+  let email;
+  if (primary_email && primary_email.email) {
+    email = primary_email.email;
+  } else if (emails.length) {
+    email = emails[0].email;
+  }
+
+  return new MemberInfo({email, fullName, rating:0, vip: false});
+};
---
+++ src/main/javascript/Deskpro/index.js
@@ -0,0 +1,1 @@
+export {parseMemberInfo} from './Parsers';
